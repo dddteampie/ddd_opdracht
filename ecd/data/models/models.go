a14b601d3ed0d7effeeb8c2560eab24279b510da
jaccovdalfsen <jvandalfsen@bettr-health.com>
2025-06-27 14:29:57 +0200

    Update foreign key references in Onderzoek, Anamnese, Diagnose, and Meetresultaat models

--- ecd/data/models/models.go
+++ ecd/data/models/models.go
@@ -24,13 +24,14 @@ type Onderzoek struct {
 	ZorgdossierID uuid.UUID       `gorm:"type:uuid"`
 	BeginDatum    time.Time       `gorm:"type:date"`
 	EindDatum     time.Time       `gorm:"type:date"`
-	Diagnose      []Diagnose      `gorm:"diagnose"`
-	Anamnese      []Anamnese      `gorm:"anamnese"`
-	Meetresultaat []Meetresultaat `gorm:"meetresultaat"`
+	Diagnose      []Diagnose      `gorm:"foreignKey:OnderzoekID;references:ID"`
+	Anamnese      []Anamnese      `gorm:"foreignKey:OnderzoekID;references:ID"`
+	Meetresultaat []Meetresultaat `gorm:"foreignKey:OnderzoekID;references:ID"`
 }
 
 type Anamnese struct {
 	ID               uuid.UUID `gorm:"type:uuid;primaryKey"`
+	OnderzoekID      uuid.UUID `gorm:"type:uuid"`
 	Klachten         string    `gorm:"klachten"`
 	DuurKlachten     string    `gorm:"duur_klachten"`
 	Medicatiegebruik string    `gorm:"medicatiegebruik"`
@@ -41,6 +42,7 @@ type Anamnese struct {
 
 type Diagnose struct {
 	ID            uuid.UUID `gorm:"type:uuid;primaryKey"`
+	OnderzoekID   uuid.UUID `gorm:"type:uuid"`
 	Diagnosecode  string    `gorm:"diagnosecode"`
 	Naam          string    `gorm:"naam"`
 	Toelichting   string    `gorm:"toelichting"`
@@ -48,9 +50,9 @@ type Diagnose struct {
 	Status        string    `gorm:"status"`
 	Geboortedatum time.Time `gorm:"type:date"`
 }
-
 type Meetresultaat struct {
 	ID             uuid.UUID `gorm:"type:uuid;primaryKey"`
+	OnderzoekID    uuid.UUID `gorm:"type:uuid"`
 	InstrumentNaam string    `gorm:"instrument_naam"`
 	Meetwaarde     string    `gorm:"meetwaarde"`
 	Datum          time.Time `gorm:"type:date"`

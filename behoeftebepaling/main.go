70eb690db44c6bc20474d06b195cf70ed98c7017
Julian <julianclayton724@gmail.com>
2025-06-28 13:03:27 +0200

    1e opzet

--- behoeftebepaling/main.go
+++ behoeftebepaling/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"behoeftebepaling/handlers"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	r.HandleFunc("/behoefte", handlers.CreateBehoefte).Methods("POST")
+	r.HandleFunc("/behoefte/onderzoek/{onderzoekId}", handlers.GetBehoefteByOnderzoekID).Methods("GET")
+	r.HandleFunc("/onderzoek/{onderzoekId}/anamnese", handlers.KoppelAnamneseHandler).Methods("POST")
+	r.HandleFunc("/onderzoek/{onderzoekId}/meetresultaat", handlers.KoppelMeetresultaatHandler).Methods("POST")
+	r.HandleFunc("/onderzoek/{onderzoekId}/diagnose", handlers.KoppelDiagnoseHandler).Methods("POST")
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
---
+++ behoeftebepaling/models/models.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Behoefte struct {
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	OnderzoekID  uuid.UUID `gorm:"type:uuid" json:"onderzoek_id"`
+	ClientID     uuid.UUID `gorm:"type:uuid" json:"client_id"`
+	Titel        string    `json:"titel"`
+	Beschrijving string    `json:"beschrijving"`
+	Urgentie     string    `json:"urgentie"`
+	Datum        time.Time `json:"datum"`
+	Onderzoek    Onderzoek `gorm:"foreignKey:OnderzoekID;references:ID"`
+	Client       Client    `gorm:"foreignKey:ClientID;references:ID"`
+}
+
+type Client struct {
+	ID            uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Naam          string    `json:"voornaam"`
+	Geboortedatum time.Time `json:"geboortedatum"`
+}
+
+type Onderzoek struct {
+	ID         uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Situatie   string    `json:"situatie"`
+	BeginDatum time.Time `json:"begin_datum"`
+	EindDatum  time.Time `json:"eind_datum"`
+}
+
+
+type AnamneseDTO struct {
+	Klachten         string `json:"klachten"`
+	Duur_Klachten    string `json:"duur_klachten"`
+	Medicatiegebruik string `json:"medicatiegebruik"`
+	Allergieën       string `json:"allergieën"`
+	Leefstijl        string `json:"leefstijl"`
+}
+
+type MeetresultaatDTO struct {
+	InstrumentNaam string    `json:"instrument_naam"`
+	Meetwaarde     string    `json:"meetwaarde"`
+	Beschrijving   string    `json:"beschrijving"`
+	Datum          time.Time `json:"datum"`
+}
+
+type DiagnoseDTO struct {
+	Naam        string    `json:"naam"`
+	Beschrijving string    `json:"beschrijving"`
+	Datum      time.Time `json:"datum"`
+}

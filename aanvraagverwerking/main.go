cbedd80bb38e9accd09c2d24486504d005cd2c15
jaccovdalfsen <jvandalfsen@bettr-health.com>
2025-07-01 18:56:19 +0200

    Also rename aanvraagverwerking folder

---
+++ aanvraagverwerking/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "aanvraagverwerking/handlers"
+    "aanvraagverwerking/pkg/config"
+    aanvraagverwerking_repo "aanvraagverwerking/repository"
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    // Load config from .env
+    cfg, err := config.LoadConfig(".env")
+    if err != nil {
+        log.Fatalf("Failed to load configuration: %v", err)
+    }
+    log.Printf("Configuration loaded")
+
+    // Initialize database
+    db, err := aanvraagverwerking_repo.InitDB(cfg.DatabaseDSN)
+    if err != nil {
+        log.Fatalf("Database initialization failed: %v", err)
+    }
+    handlers.InitHandlers(db) // als je dependency injection gebruikt
+
+    r := mux.NewRouter()
+    r.HandleFunc("/aanvraag", handlers.StartAanvraag).Methods("POST")
+    r.HandleFunc("/aanvraag/{id}", handlers.GetAanvraagByID).Methods("GET")
+    r.HandleFunc("/aanvraag/client/{clientId}", handlers.GetAanvragenByClientID).Methods("GET")
+    r.HandleFunc("/aanvraag/categorie/kies", handlers.KiesCategorie).Methods("POST")
+
+	
+    log.Printf("Behoeftebepaling-service draait op %s...", cfg.ServerPort)
+    log.Fatal(http.ListenAndServe(cfg.ServerPort, r))
+}

b2a0b18aefe7eded403f07f227f8fe96a2f2a151
Julian <julianclayton724@gmail.com>
2025-07-04 16:07:54 +0200

    dq

--- tests/init-multiple-dbs.sql
+++ tests/init-multiple-dbs.sql
@@ -1,5 +1,20 @@
-CREATE DATABASE ecd;
-CREATE DATABASE behoeftebepaling;
-CREATE DATABASE aanvraagverwerking;
-CREATE DATABASE products;
-CREATE DATABASE recommendation;
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ecd') THEN
+      CREATE DATABASE ecd;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'behoeftebepaling') THEN
+      CREATE DATABASE behoeftebepaling;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'aanvraagverwerking') THEN
+      CREATE DATABASE aanvraagverwerking;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'products') THEN
+      CREATE DATABASE products;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'recommendation') THEN
+      CREATE DATABASE recommendation;
+   END IF;
+END
+$$;
